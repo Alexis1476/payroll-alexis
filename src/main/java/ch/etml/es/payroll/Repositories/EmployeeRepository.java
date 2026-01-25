87940111911ca55843aaef5a6591e390d9b8cf5f
Nicolas Glassey <nicolas.glassey@eduvaud.ch>
2026-01-25 18:40:06 +0100

    chore: initial commit - base code

---
+++ src/main/java/ch/etml/es/payroll/Repositories/EmployeeRepository.java
@@ -0,0 +1,7 @@
+package ch.etml.es.payroll.Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<ch.etml.es.payroll.Entities.Employee, Long>{
+
+}
