d2113f22699865533d75a3dc176ddecf1979df61
Nicolas Glassey <nicolas.glassey@eduvaud.ch>
2026-03-08 18:35:42 +0100

    refactor: departments is now correctly named as a REST resource

--- src/main/java/ch/etml/es/payroll/controllers/DepartmentNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package ch.etml.es.payroll.controllers;
-
-public class DepartmentNotFoundException extends RuntimeException{
-
-    DepartmentNotFoundException(Long id){
-        super("Could not find department " + id);
-    }
-}
