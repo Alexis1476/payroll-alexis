35c0813266fea3ee136e3cd89ee11ef57fa4d6c4
Nicolas Glassey <nicolas.glassey@eduvaud.ch>
2026-03-08 18:35:42 +0100

    refactor: departments is now correclty named as a REST resource

--- src/main/java/ch/etml/es/payroll/controllers/DepartmentAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package ch.etml.es.payroll.controllers;
-
-public class DepartmentAlreadyExistsException extends RuntimeException{
-
-    public DepartmentAlreadyExistsException(String acronym){
-        super("Department " + acronym + " already exists");}
-}
