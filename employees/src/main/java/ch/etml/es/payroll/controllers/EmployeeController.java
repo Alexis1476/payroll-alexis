b7e5c9289d59c0e805f0f87205c2aa1029852ea4
Alexis1476 <halexisr20@hotmail.com>
2026-03-09 13:22:00 +0100

    feat(employee): delete employee now available

--- employees/src/main/java/ch/etml/es/payroll/controllers/EmployeeController.java
+++ employees/src/main/java/ch/etml/es/payroll/controllers/EmployeeController.java
@@ -56,4 +56,13 @@ public class EmployeeController {
                 .created(location)
                 .body(created);
     }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteEmployee(@PathVariable Long id){
+        Employee employee = repository.findById(id)
+                .orElseThrow(() -> new EmployeeNotFoundException(id));
+
+        repository.delete(employee);
+        return ResponseEntity.noContent().build();
+    }
 }
