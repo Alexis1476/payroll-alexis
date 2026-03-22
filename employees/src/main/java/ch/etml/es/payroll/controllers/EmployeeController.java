01516b87aef32f2b379a5fcdbdf27db349d72bc5
Nicolas Glassey <nicolas.glassey@eduvaud.ch>
2026-03-22 17:30:58 +0100

    feat(employee): delete method is now supported

--- employees/src/main/java/ch/etml/es/payroll/controllers/EmployeeController.java
+++ employees/src/main/java/ch/etml/es/payroll/controllers/EmployeeController.java
@@ -56,4 +56,19 @@ public class EmployeeController {
                 .created(location)
                 .body(created);
     }
+
+    /* curl sample :
+        curl -i -X DELETE localhost:8080/api/v1/employees/2
+        */
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteEmployee(@PathVariable Long id) {
+
+        if (!repository.existsById(id)) {
+            throw new EmployeeNotFoundException(id);
+        }
+
+        repository.deleteById(id);
+
+        return ResponseEntity.noContent().build();
+    }
 }
